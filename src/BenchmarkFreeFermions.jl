555c5cbaa5dfdec5f8289c1edfaf39ddd89be2cc
qyli <qiaoyili@buaa.edu.cn>
2024-10-10 19:56:51 +0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.49

---
+++ src/BenchmarkFreeFermions.jl
@@ -0,0 +1,5 @@
+module BenchmarkFreeFermions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using BenchmarkFreeFermions
+using Test
+
+@testset "BenchmarkFreeFermions.jl" begin
+    # Write your tests here.
+end
